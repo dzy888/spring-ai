f2c97476aa14443d1e9aa89a9c38f1d495b03d56
daizhengyu <1452035653@qq.com>
2025-02-25 17:03:36 +0800

    测试

---
+++ spring-ai-core/src/test/java/org/springframework/ai/chat/client/advisor/VectorStoreChatMemoryAdvisorTests.java
@@ -0,0 +1,23 @@
+package org.springframework.ai.chat.client.advisor;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * @Author dzy
+ * @Date 2025/2/25 16:53
+ * @PackageName:org.springframework.ai.chat.client.advisor
+ * @ClassName: VectorStoreChatMemoryAdvisorTests
+ * @Description: TODO
+ * @Version 1.0
+ */
+@ExtendWith(MockitoExtension.class)
+public class VectorStoreChatMemoryAdvisorTests {
+
+	@Test
+	public void vectorStoreChatMemoryAdvisor() {
+		// TODO
+
+	}
+}
